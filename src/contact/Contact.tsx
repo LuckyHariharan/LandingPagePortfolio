ca92368f51ec67885b9e884957901ac346d25268
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2023-12-20 23:31:05 -0500

    add button spacing

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -27,7 +27,7 @@ const Contact = ({ button }: ContactProps) => {
             id="message"
             className="h-40 w-full border-4 p-2"
           ></textarea>
-          <div className="flex justify-between px-12 md:mr-12 md:space-x-12">
+          <div className="flex justify-between space-x-8 px-12 md:mr-12 md:space-x-12">
             <div className="flex justify-center">{button}</div>
             <div className="flex items-center justify-center rounded-md border-2 border-slate-500 px-2 text-center text-lg font-normal hover:bg-slate-500 hover:text-white">
               <button
